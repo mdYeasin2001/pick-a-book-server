a32c69d6fcc61a607f2bb3743d988b4cbc0faecb
Yeasin <yeasin30112001@gmail.com>
2021-03-31 07:10:40 -0600

    port updated

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 const port = 8080;
 
 
-
+app.use(express.json());
+app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.54hym.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("BookStore").collection("books");
-  console.log('database connected');
+  const booksCollection = client.db("BookStore").collection("books");
+
+  app.post('/addBook', (req, res) => {
+      const newBook = req.body;
+      booksCollection.insertOne(newBook)
+      .then(result => console.log(result))
+  })
+
+  app.get('/books', (req, res) => {
+      booksCollection.find()
+      .toArray((err, books) => {
+          res.send(books)
+      })
+  })
 });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port)
+app.listen(process.env.PORT || port)
